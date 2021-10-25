0f4b521d752465905ce8065b24603ab7f60c300b
abhisheksaran <abhishekbishnoi695@gmail.com>
2021-10-25 15:34:27 +0530

    argparse added

--- grafana_backup.py
+++ grafana_backup.py
@@ -3,6 +3,8 @@ import os
 import json
 import grafana_sdk
 import glob
+import argparse
+import pytz 
 #import boto3
 from datetime import datetime
 
@@ -15,7 +17,10 @@ class GrafanaBackupManager:
         self.name = name
         self.grafana_api = grafana_sdk.GrafanaApi(grafana_url, api_key)
         self.s3 = True
-        current_date = datetime.now().strftime("%d-%m-%YT%H:%M:%S")
+
+        # Standardizing time in IST = UTC + 05:30
+        IST = pytz.timezone('Asia/Kolkata')
+        current_date = datetime.now(IST).strftime("%d-%m-%YT%H:%M:%S")
         self.folder_name = current_date+'/'
 
         # Get grafana config from environment variables
@@ -24,9 +29,7 @@ class GrafanaBackupManager:
         self.local = local_backup_content.get('enabled', True) == True
 
         if self.local:
-            self.backup_folder = local_backup_content.get('backup_folder', '')+'/'+self.name+'/daily/'
-            grafana_sdk.get_logger().info("Local backup is enabled and storing under : {}".format(self.backup_folder))
-            grafana_sdk.get_logger().info("Each Backup folder will have format like this : {}".format(current_date))
+            self.parent_backup_folder = local_backup_content.get('backup_folder', '')+'/'+self.name+'/daily/'
             
 
     def dashboard_backup(self):
@@ -34,19 +37,24 @@ class GrafanaBackupManager:
         Get all the dashboards in all folder 
         """
         try:
-            dashboards = self.grafana_api.search_db()
-            if len(dashboards) == 0:
-                grafana_sdk.get_logger().info("Could not find any data for backup under")
-            else:
-                grafana_sdk.get_logger().info("Scanned {} dashboards for backup -".format(len(dashboards)))
+                
+            if self.local:
+                grafana_sdk.get_logger().info("Local backup is enabled and storing under : {}".format(self.parent_backup_folder))
+                grafana_sdk.get_logger().info("Each Backup folder will have format like this : %d-%m-%YT%H:%M:%S")
+        
+                dashboards = self.grafana_api.search_db()
+                if len(dashboards) == 0:
+                    grafana_sdk.get_logger().info("Could not find any data for backup under")
+                else:
+                    grafana_sdk.get_logger().info("Scanned {} dashboards for backup -".format(len(dashboards)))
 
-            for dashboard in dashboards:
-                dashboard_uri = dashboard['uid']
-                dashboard_title = dashboard['title'].replace(" ","")
-                dashboard_details_json = self.grafana_api.dashboard_details(dashboard_uri)
-                # Store the dashboard in the folder_name location
-                folder_name = self.backup_folder+self.folder_name
-                self.__store(folder_name, "{}_{}.json".format(dashboard_title.lower(), dashboard_uri.lower()), dashboard_details_json)
+                for dashboard in dashboards:
+                    dashboard_uri = dashboard['uid']
+                    dashboard_title = dashboard['title'].replace(" ","")
+                    dashboard_details_json = self.grafana_api.dashboard_details(dashboard_uri)
+                    # Store the dashboard in the folder_name location
+                    folder_name = self.parent_backup_folder+self.folder_name
+                    self.__store(folder_name, "{}_{}.json".format(dashboard_title.lower(), dashboard_uri.lower()), dashboard_details_json)
         except Exception as exc:
             grafana_sdk.get_logger().info("Error taking backup, error : {}".format(str(exc)))
 
@@ -65,21 +73,29 @@ class GrafanaBackupManager:
         except Exception as exc:
             grafana_sdk.get_logger().info("Error storing backup locally, error : {}".format( str(exc)))
 
-    def dashboard_restore(self):
+    def dashboard_restore(self, backup_folder):
         """
-        Restore all the dashboards from the recent backup
+        Restore all the dashboards from the given backup folder, if no folder specified, we will restore from the most recent backup. 
         """
         try:
-            # Restore most recent backup
-            most_recent_backup = max(glob.glob(os.path.join(self.backup_folder, '*/')), key=os.path.getmtime)
-            grafana_sdk.get_logger().info("Most recent backup is {}".format(most_recent_backup))
-            backup_file_list = os.listdir(most_recent_backup)
+            # If a backup_folder is not specified, restore the most recent backup
+            if backup_folder is None:
+                backup_folder = max(glob.glob(os.path.join(self.parent_backup_folder, '*/')), key=os.path.getmtime)
+                grafana_sdk.get_logger().info("Restoring from recent backup: {}".format(backup_folder))
+                backup_file_list = os.listdir(backup_folder)
+            else:
+                backup_folder = self.parent_backup_folder+backup_folder+'/'
+                grafana_sdk.get_logger().info("Restoring from the specified backup: {}".format(backup_folder))
+                backup_file_list = os.listdir(backup_folder)
+        
+            #print(backup_folder)
+
             if len(backup_file_list) == 0:
-                grafana_sdk.get_logger().info("Couldn't find any files to restore in folder {}".format(self.backup_folder+self.folder_name))
+                grafana_sdk.get_logger().info("Couldn't find any files to restore in folder {}".format(self.parent_backup_folder+backup_folder))
             else:
                 grafana_sdk.get_logger().info("Scanned data to create db- {}".format(backup_file_list))
             for backup_file in backup_file_list:
-                dashboard_content_json = self.get_backup_meta_content(most_recent_backup+backup_file)
+                dashboard_content_json = self.get_backup_meta_content(backup_folder+backup_file)
                 folder_id = dashboard_content_json['meta']['folderId']
                 folder_title = dashboard_content_json['meta']['folderTitle']
                 if folder_id != 0:
@@ -94,9 +110,11 @@ class GrafanaBackupManager:
                 del dashboard_content_json['dashboard']['id']
                 dashboard_content_json['folderId'] = folder_id
                 dashboard_content_json['message'] = "Restored the dashboard with backup file {} on {}".format(backup_file, datetime.now().strftime("%d-%m-%YT%H:%M:%S"))
-                # We only want to restore deleted dashboards 
-                dashboard_content_json['overwrite'] = False
-                grafana_sdk.get_logger().info("\n\n")
+                
+                # Overwrite property is needs to be discussed
+                dashboard_content_json['overwrite'] = True
+                
+                #grafana_sdk.get_logger().info("\n\n")
                 response = self.grafana_api.restore(json.dumps(dashboard_content_json))
                 if response.status_code == 200:
                     grafana_sdk.get_logger().info("Restored the dashboard with backup file {}".format(backup_file))
@@ -136,7 +154,7 @@ class GrafanaBackupManager:
                     }
                 }
             }
-            print(grafana_url_data)
+            #print(grafana_url_data)
             return grafana_url_data
         except Exception as exc:
             grafana_sdk.get_logger().info("Error getting grafana-config env variables, error: {}".format(str(exc)))
@@ -154,18 +172,36 @@ def get_grafana_mapper(grafana_url):
     except Exception as exc:
         grafana_sdk.get_logger().info("Error mapping grafana host config file, {}".format(str(exc)))
 
-if __name__=="__main__":
-    # Get the url file
+def main():
+    """
+    Main method to drive the backup and restore. 
+    """
+    # Argument parser
+    parser = argparse.ArgumentParser(description='Grafana backup and restoration script.') 
+    group = parser.add_mutually_exclusive_group(required=True)
+
+    group.add_argument('-b','--backup', action='store_true',help="take a backup of all the dashboards")
+    group.add_argument('-r','--restore', type=str, nargs='?', default='',  metavar='BACKUP_DIRECTORY', help="restore all the dashboard from most recent backup, specify a \"backup_directory\" if you want to restore a particular backup. You can find last five backup directories in the backup logs.")
+    
+    args = parser.parse_args()
+    print(args)    
+    # Get the env variables
     grafana_url = GrafanaBackupManager.get_grafana_content()
+    #grafana_sdk.get_logger().info("The grafana configuration is: {}".format(grafana_url))
     name, url, api_key = get_grafana_mapper(grafana_url['grafana_urls'][0])
     gbm = GrafanaBackupManager(name, url, api_key)
 
-    #if input("Enter 'backup' for backup and 'restore' for restore\n") == 'backup':
-    #    gbm.dashboard_backup()
-    #else:
-    gbm.dashboard_restore()
-    #gbm.dashboard_backup()
 
+    if args.backup:
+        grafana_sdk.get_logger().info("Recieved input -b for backup ")
+        gbm.dashboard_backup()        
+    elif 'restore' in args:
+        grafana_sdk.get_logger().info("Recieved input -r for  restore ")
+        gbm.dashboard_restore(args.restore)
+    else:
+        parser.error('No action requested, add -b for backup or -r for restore.')
 
-        
+if __name__=="__main__":
+    main()
+           
 
